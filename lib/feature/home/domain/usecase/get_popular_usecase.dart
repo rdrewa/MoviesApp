a195ad1cd98283bbf69f1619a4a9d1a40668b2a3
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-16 02:49:49 +0100

    MBA-1: First empty usecases

---
+++ lib/feature/home/domain/usecase/get_popular_usecase.dart
@@ -0,0 +1,6 @@
+import 'package:movies_app/feature/common/domain/model/movie.dart';
+
+class GetPopularUsecase {
+
+  Future<List<Movie>> call() async => [];
+}
---
+++ lib/feature/home/domain/usecase/get_trending_usecase.dart
@@ -0,0 +1,6 @@
+import 'package:movies_app/feature/common/domain/model/movie.dart';
+
+class GetTrendingUsecase {
+
+  Future<List<Movie>> call() async => [];
+}
