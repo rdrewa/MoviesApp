023c8e343ec1c282402bd6fe5a1232ed278844bc
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-16 02:58:03 +0100

    MBA-1: Separated App widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app/movie_app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Movie BLoC App'),
-        ),
-      ),
-    );
-  }
+  runApp(const MovieApp());
 }
