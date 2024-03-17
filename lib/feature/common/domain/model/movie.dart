4de9a7e7ebe0c702cd5a0094f0d18320ce02a1d0
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-17 18:10:14 +0100

    MBA-3: Remove some fields; Methods for reading title, originalTitle & releaseDate from alternative fields

--- lib/feature/common/domain/model/movie.dart
+++ lib/feature/common/domain/model/movie.dart
@@ -1,88 +1,57 @@
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import 'media_type.dart';
-
 part 'movie.g.dart';
 
 @JsonSerializable()
 class Movie extends Equatable {
   final bool adult;
   final String? backdropPath;
+  final List<int> genreIds;
   final int id;
-  final String? title;
-  final String? originalLanguage;
-  final String? originalTitle;
-  final String? overview;
-  final String? posterPath;
-  final MediaType mediaType;
-  final List<int>? genreIds;
+  final String originalLanguage;
+  @JsonKey(readValue: _readOriginalTitle)
+  final String originalTitle;
+  final String overview;
   final double popularity;
-  final String? releaseDate;
-  final bool? video;
-  final double? voteAverage;
-  final int? voteCount;
-  final String? name;
-  final String? originalName;
-  final DateTime? firstAirDate;
-  final List<String>? originCountry;
-  final int? gender;
-  final String? knownForDepartment;
-  final dynamic profilePath;
-  final List<Movie>? knownFor;
+  final String posterPath;
+  @JsonKey(readValue: _readReleaseDate)
+  final DateTime releaseDate;
+  @JsonKey(readValue: _readTitle)
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
 
   const Movie(
       {required this.adult,
-      required this.backdropPath,
+      this.backdropPath,
+      required this.genreIds,
       required this.id,
-      required this.title,
       required this.originalLanguage,
       required this.originalTitle,
       required this.overview,
-      required this.posterPath,
-      required this.mediaType,
-      required this.genreIds,
       required this.popularity,
+      required this.posterPath,
       required this.releaseDate,
+      required this.title,
       required this.video,
       required this.voteAverage,
-      required this.voteCount,
-      required this.name,
-      required this.originalName,
-      required this.firstAirDate,
-      required this.originCountry,
-      required this.gender,
-      required this.knownForDepartment,
-      required this.profilePath,
-      required this.knownFor});
+      required this.voteCount});
 
   factory Movie.fromJson(Map<String, dynamic> data) => _$MovieFromJson(data);
 
   Map<String, dynamic> toJson() => _$MovieToJson(this);
 
   @override
-  List<Object?> get props => [
-        adult,
-        backdropPath,
-        id,
-        title,
-        originalLanguage,
-        originalTitle,
-        overview,
-        posterPath,
-        mediaType,
-        popularity,
-        releaseDate,
-        video,
-        voteAverage,
-        voteCount,
-        name,
-        originalName,
-        firstAirDate,
-        originCountry,
-        gender,
-        knownForDepartment,
-        profilePath,
-        knownFor
-      ];
+  List<Object?> get props => [id];
+
+  static Object _readOriginalTitle(Map<dynamic, dynamic> map, String key) =>
+      map[key] ?? (map['original_name'] ?? '');
+
+  static Object _readReleaseDate(Map<dynamic, dynamic> map, String key) =>
+      map[key] ?? (map['first_air_date'] ?? '');
+
+  static Object _readTitle(Map<dynamic, dynamic> map, String key) =>
+      map[key] ?? (map['name'] ?? '');
 }
