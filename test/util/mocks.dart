253bceda385a4ca203f4511e732999f731842a3b
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-19 18:12:13 +0100

    MBA-7: Mock for HttpClientAdapter

--- test/util/mocks.dart
+++ test/util/mocks.dart
@@ -1,5 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:mocktail/mocktail.dart';
 
 import 'package:movies_app/feature/common/domain/repository/movie_repository.dart';
 
 class MockMovieRepository extends Mock implements MovieRepository {}
+
+class MockHttpClientAdapter extends Mock implements HttpClientAdapter {}
