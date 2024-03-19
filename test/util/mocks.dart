ecf17e4bdcb505dd4146b40f3251d2c61a0aa734
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-19 23:45:07 +0100

    MBA-9: MovieService mock added

--- test/util/mocks.dart
+++ test/util/mocks.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:mocktail/mocktail.dart';
 
+import 'package:movies_app/feature/common/data/source/remote/movie_service.dart';
 import 'package:movies_app/feature/common/domain/repository/movie_repository.dart';
 
 class MockMovieRepository extends Mock implements MovieRepository {}
 
 class MockHttpClientAdapter extends Mock implements HttpClientAdapter {}
+
+class MockMovieService extends Mock implements MovieService {}
