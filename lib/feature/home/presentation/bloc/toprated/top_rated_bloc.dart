9a6a0573e99ea440b004057773c8fcbfddde7244
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-20 09:36:39 +0100

    MBA-11: TopRatedBloc classes (bloc, event, state) added

---
+++ lib/feature/home/presentation/bloc/toprated/top_rated_bloc.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../common/domain/model/movie.dart';
+import '../../../domain/usecase/get_top_rated_usecase.dart';
+
+part 'top_rated_event.dart';
+part 'top_rated_state.dart';
+
+class TopRatedBloc extends Bloc<TopRatedEvent, TopRatedState> {
+  final GetTopRatedUsecase _getTopRatedUsecase;
+
+  TopRatedBloc(this._getTopRatedUsecase) : super(TopRatedInitial()) {
+    on<TopRatedEvent>(_onGetTopRatedEvent);
+  }
+
+  FutureOr<void> _onGetTopRatedEvent(
+      TopRatedEvent event, Emitter<TopRatedState> emit) async {
+    emit(TopRatedLoading());
+
+    (await _getTopRatedUsecase()).fold(
+        (fail) => emit(const TopRatedFailure('Something went wrong')),
+        (data) => emit(data.isEmpty ? TopRatedEmpty() : TopRatedLoaded(data)));
+  }
+}
