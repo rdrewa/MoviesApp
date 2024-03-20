f4e1eba1b5ebe543fd7db93038fa28c8919ba699
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-20 09:32:14 +0100

    MBA-11: Mocking Usecase from Home

--- test/util/mocks.dart
+++ test/util/mocks.dart
@@ -3,9 +3,18 @@ import 'package:mocktail/mocktail.dart';
 
 import 'package:movies_app/feature/common/data/source/remote/movie_service.dart';
 import 'package:movies_app/feature/common/domain/repository/movie_repository.dart';
+import 'package:movies_app/feature/home/domain/usecase/get_popular_usecase.dart';
+import 'package:movies_app/feature/home/domain/usecase/get_top_rated_usecase.dart';
+import 'package:movies_app/feature/home/domain/usecase/get_trending_usecase.dart';
 
 class MockMovieRepository extends Mock implements MovieRepository {}
 
 class MockHttpClientAdapter extends Mock implements HttpClientAdapter {}
 
 class MockMovieService extends Mock implements MovieService {}
+
+class MockGetTrendingUsecase extends Mock implements GetTrendingUsecase {}
+
+class MockGetTopRatedUsecase extends Mock implements GetTopRatedUsecase {}
+
+class MockGetPopularUsecase extends Mock implements GetPopularUsecase {}
