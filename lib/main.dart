dbe97d1f6b944d80cea7af49dbbe3a650aac197b
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-21 13:13:21 +0100

    MBA-13: Load config data from .env; Initialise dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'app/di.dart';
 import 'app/movie_app.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
+  initDependencies();
   runApp(const MovieApp());
 }
