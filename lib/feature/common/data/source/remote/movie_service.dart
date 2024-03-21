edce18e3fe6b83601e143127677e4ea400a27d01
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-21 13:17:21 +0100

    MBA-13: Using baseUrl from Config; Changed endpoints

--- lib/feature/common/data/source/remote/movie_service.dart
+++ lib/feature/common/data/source/remote/movie_service.dart
@@ -5,16 +5,16 @@ import '../../../../common/data/model/response_data.dart';
 
 part 'movie_service.g.dart';
 
-@RestApi(baseUrl: 'https://api.themoviedb.org/3')
+@RestApi()
 abstract class MovieService {
   factory MovieService(Dio dio) = _MovieService;
 
-  @GET('/trending/all/week?api_key={apiKey}')
+  @GET('trending/all/week?api_key={apiKey}')
   Future<ResponseData> getTrendingList(@Path() String apiKey);
 
-  @GET('/movie/popular?api_key={apiKey}')
+  @GET('movie/popular?api_key={apiKey}')
   Future<ResponseData> getPopularList(@Path() String apiKey);
 
-  @GET('/movie/top_rated?api_key={apiKey}')
+  @GET('movie/top_rated?api_key={apiKey}')
   Future<ResponseData> getTopRatedList(@Path() String apiKey);
 }
