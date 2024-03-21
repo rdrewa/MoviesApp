ffe4b532364f10189c3f9e17594a9786abc8c2a9
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-21 13:14:29 +0100

    MBA-13: Providing Blocs

--- lib/app/movie_app.dart
+++ lib/app/movie_app.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/feature/home/presentation/screen/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../feature/home/presentation/bloc/popular/popular_bloc.dart';
+import '../feature/home/presentation/bloc/toprated/top_rated_bloc.dart';
+import '../feature/home/presentation/bloc/trending/trending_bloc.dart';
+import '../feature/home/presentation/screen/home_screen.dart';
+
+import 'di.dart';
 
 class MovieApp extends StatelessWidget {
   const MovieApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: HomeScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (_) => sl<TrendingBloc>()..add(GetTrendingEvent())),
+        BlocProvider(
+            create: (_) => sl<PopularBloc>()..add(GetPopularEvent())),
+        BlocProvider(
+            create: (_) => sl<TopRatedBloc>()..add(GetTopRatedEvent()))
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: HomeScreen(),
+        ),
       ),
     );
   }
