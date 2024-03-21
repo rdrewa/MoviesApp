33a2cd9d75036d9ea2788f6dae94de57eb4c118b
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-21 13:18:38 +0100

    MBA-13: Temporary using only one Bloc, just for launching

--- lib/feature/home/presentation/screen/home_screen.dart
+++ lib/feature/home/presentation/screen/home_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../presentation/bloc/trending/trending_bloc.dart';
+import 'movie_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Movie BLoC App: Home'),
-    );
+    return SafeArea(
+        child: BlocBuilder<TrendingBloc, TrendingState>(
+            builder: (context, state) => switch (state) {
+                  TrendingInitial() => const Center(
+                      child: Text('Movie BLoC App: Home'),
+                    ),
+                  TrendingEmpty() => const Text('Empty Trending Movies list'),
+                  TrendingLoading() => const CircularProgressIndicator(),
+                  TrendingLoaded() => ListView.builder(
+                      itemCount: state.data.length,
+                      itemBuilder: (BuildContext context, int index) =>
+                          MovieItem(data: state.data[index])),
+                  TrendingFailure() => Text(state.message),
+                }));
   }
 }
