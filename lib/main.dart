52bd58694ecde173f8b5961316c2472343a63db1
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-21 15:31:37 +0100

    MBA-15: Initialized localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movies_app/core/util/language_util.dart';
 
 import 'app/di.dart';
 import 'app/movie_app.dart';
@@ -7,5 +9,12 @@ import 'app/movie_app.dart';
 void main() async {
   await dotenv.load(fileName: '.env');
   initDependencies();
-  runApp(const MovieApp());
+  await EasyLocalization.ensureInitialized();
+
+  runApp(EasyLocalization(
+      supportedLocales: LanguageUtil.supportedLocales,
+      path: 'assets/i18n',
+      fallbackLocale: LanguageUtil.locale,
+      useOnlyLangCode: true,
+      child: const MovieApp()));
 }
