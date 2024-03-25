bed241aa43d47e8c3cc6c62f1961c8560d8805d1
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-25 15:38:02 +0100

    MBA-21: Model Cast and test + fixture

---
+++ lib/feature/details/domain/model/cast.dart
@@ -0,0 +1,66 @@
+import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'cast.g.dart';
+
+@JsonSerializable()
+class Cast extends Equatable {
+  final bool adult;
+  final int gender;
+  final int id;
+  @JsonKey(name: 'known_for_department')
+  final String knownForDepartment;
+  final String name;
+  @JsonKey(name: 'original_name')
+  final String originalName;
+  final double popularity;
+  @JsonKey(name: 'profile_path')
+  final String? profilePath;
+  @JsonKey(name: 'cast_id')
+  final int? castId;
+  final String? character;
+  @JsonKey(name: 'credit_id')
+  final String creditId;
+  final int? order;
+  final String? department;
+  final String? job;
+
+  const Cast({
+    required this.adult,
+    required this.gender,
+    required this.id,
+    required this.knownForDepartment,
+    required this.name,
+    required this.originalName,
+    required this.popularity,
+    required this.profilePath,
+    this.castId,
+    this.character,
+    required this.creditId,
+    this.order,
+    this.department,
+    this.job,
+  });
+
+  factory Cast.fromJson(Map<String, dynamic> data) => _$CastFromJson(data);
+
+  Map<String, dynamic> toJson() => _$CastToJson(this);
+
+  @override
+  List<Object?> get props => [
+        adult,
+        gender,
+        id,
+        knownForDepartment,
+        name,
+        originalName,
+        popularity,
+        profilePath,
+        castId,
+        character,
+        creditId,
+        order,
+        department,
+        job
+      ];
+}
