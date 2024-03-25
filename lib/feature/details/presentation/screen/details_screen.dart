2d5b1dc3f257cc8f5aa07736fd7d667685b2c8a1
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-25 23:14:45 +0100

    MBA-21: Bloc used

--- lib/feature/details/presentation/screen/details_screen.dart
+++ lib/feature/details/presentation/screen/details_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../common/presentation/widget/progress_wheel.dart';
+import '../../../../app/di.dart';
+import '../bloc/details/details_bloc.dart';
+import '../widget/details_container.dart';
 
 class DetailsScreen extends StatelessWidget {
   final int id;
@@ -6,9 +12,16 @@ class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key, required this.id});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Details [$id] Screen'),
-      ));
+  Widget build(BuildContext context) => BlocProvider(
+    create: (context) => sl<DetailsBloc>()..add(GetDetailsEvent(id)),
+    child: Scaffold(
+        appBar: AppBar(),
+        body: BlocBuilder<DetailsBloc, DetailsState>(
+            builder: (context, state) => switch (state) {
+              DetailsInitial() => const SizedBox.shrink(),
+              DetailsLoading() => const ProgressWheel(),
+              DetailsLoaded() => DetailsContainer(details: state.data),
+              DetailsFailure() => Text(state.message),
+            })),
+  );
 }
