b3fb77edd49ae7a5c6a4f6db7d9e635c91057d81
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-25 15:38:15 +0100

    MBA-21: Model Credits and test + fixture

---
+++ lib/feature/details/domain/model/credits.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'cast.dart';
+
+part 'credits.g.dart';
+
+@JsonSerializable()
+class Credits extends Equatable {
+  final List<Cast> cast;
+  final List<Cast> crew;
+
+  const Credits({
+    required this.cast,
+    required this.crew,
+  });
+
+  factory Credits.fromJson(Map<String, dynamic> data) => _$CreditsFromJson(data);
+
+  Map<String, dynamic> toJson() => _$CreditsToJson(this);
+
+  @override
+  List<Object?> get props => [cast, crew];
+}
