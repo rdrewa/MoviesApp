4550f3800663c1ddfb44cf28368e7053820b30ce
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-26 20:21:24 +0100

    MBA-23: Mocking Watch usecases, repository & service

--- test/util/mocks.dart
+++ test/util/mocks.dart
@@ -1,9 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:mocktail/mocktail.dart';
 
+import 'package:movies_app/feature/common/data/source/local/watch_service.dart';
 import 'package:movies_app/feature/common/data/source/remote/movie_service.dart';
 import 'package:movies_app/feature/common/domain/repository/movie_repository.dart';
+import 'package:movies_app/feature/common/domain/repository/watch_repository.dart';
+import 'package:movies_app/feature/details/domain/usecase/add_to_watch_usecase.dart';
 import 'package:movies_app/feature/details/domain/usecase/get_details_usecase.dart';
+import 'package:movies_app/feature/details/domain/usecase/is_on_watch_usecase.dart';
+import 'package:movies_app/feature/details/domain/usecase/remove_from_watch_usecase.dart';
 import 'package:movies_app/feature/home/domain/usecase/get_popular_usecase.dart';
 import 'package:movies_app/feature/home/domain/usecase/get_top_rated_usecase.dart';
 import 'package:movies_app/feature/home/domain/usecase/get_trending_usecase.dart';
@@ -21,3 +26,13 @@ class MockGetTopRatedUsecase extends Mock implements GetTopRatedUsecase {}
 class MockGetPopularUsecase extends Mock implements GetPopularUsecase {}
 
 class MockGetDetailsUsecase extends Mock implements GetDetailsUsecase {}
+
+class MockWatchService extends Mock implements WatchService {}
+
+class MockWatchRepository extends Mock implements WatchRepository {}
+
+class MockAddToWatchUsecase extends Mock implements AddToWatchUsecase {}
+
+class MockRemoveFromWatchUsecase extends Mock implements RemoveFromWatchUsecase {}
+
+class MockIsOnWatchUsecase extends Mock implements IsOnWatchUsecase {}
