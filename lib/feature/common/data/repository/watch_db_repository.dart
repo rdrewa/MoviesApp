7d2781cca7d08a256fefeb67521c26a20a31c642
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-26 20:22:46 +0100

    MBA-23: WatchRepository implementations added

---
+++ lib/feature/common/data/repository/watch_db_repository.dart
@@ -0,0 +1,54 @@
+import 'package:dartz/dartz.dart';
+import 'package:hive/hive.dart';
+
+import '../../../../core/error/failure.dart';
+import '../../data/source/local/watch_service.dart';
+import '../../domain/model/movie.dart';
+import '../../domain/repository/watch_repository.dart';
+
+class WatchDbRepository implements WatchRepository {
+  final WatchService watchService;
+
+  WatchDbRepository(this.watchService);
+
+  @override
+  Future<Either<Failure, void>> addItem(Movie item) async {
+    try {
+      await watchService.addItem(item);
+      return const Right(null);
+    } on HiveError catch (failure) {
+      return Left(DatabaseFailure(failure.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> isAdded(int id) async {
+    try {
+      Movie? item = await watchService.getItem(id);
+      return Right(item != null);
+    } on HiveError catch (failure) {
+      return Left(DatabaseFailure(failure.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Movie>>> getList() async {
+    try {
+      final data = await watchService.getList();
+      return Right(data);
+    } on HiveError catch (failure) {
+      return Left(DatabaseFailure(failure.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> removeItem(Movie item) async {
+    try {
+      await watchService.removeItem(item);
+      return const Right(null);
+    } on HiveError catch (failure) {
+      return Left(DatabaseFailure(failure.message));
+    }
+  }
+
+}
