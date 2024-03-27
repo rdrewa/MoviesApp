a0d284f8809c8bae40a3e67e625bc4de8504c7c0
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-27 21:40:25 +0100

    MBA-27: Temporary Now widgets

---
+++ lib/feature/now/presentation/widget/now_item.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+import '../../domain/model/movie_now.dart';
+
+class NowItem extends StatelessWidget {
+  final MovieNow item;
+
+  const NowItem({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(item.title);
+  }
+}
