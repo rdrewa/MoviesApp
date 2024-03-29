f531aa159044fa631adf51018e8aeee5d6ff8aac
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-29 15:10:30 +0100

    MBA-29: Moved bloc into HomeScreen

--- lib/app/movie_app.dart
+++ lib/app/movie_app.dart
@@ -1,34 +1,19 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../feature/home/presentation/bloc/popular/popular_bloc.dart';
-import '../feature/home/presentation/bloc/toprated/top_rated_bloc.dart';
-import '../feature/home/presentation/bloc/trending/trending_bloc.dart';
 import 'app_router.dart';
-
-import 'di.dart';
 
 class MovieApp extends StatelessWidget {
   const MovieApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider(
-              create: (_) => sl<TrendingBloc>()..add(GetTrendingEvent())),
-          BlocProvider(
-              create: (_) => sl<PopularBloc>()..add(GetPopularEvent())),
-          BlocProvider(
-              create: (_) => sl<TopRatedBloc>()..add(GetTopRatedEvent()))
-        ],
-        child: MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          routerConfig: AppRouter.config,
-          localizationsDelegates: context.localizationDelegates,
-          supportedLocales: context.supportedLocales,
-          locale: context.locale,
-        ));
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: AppRouter.config,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+    );
   }
 }
--- lib/feature/home/presentation/screen/home_screen.dart
+++ lib/feature/home/presentation/screen/home_screen.dart
@@ -2,8 +2,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../app/di.dart';
 import '../../../common/presentation/widget/progress_wheel.dart';
-import '../../presentation/bloc/trending/trending_bloc.dart';
+import '../bloc/trending/trending_bloc.dart';
+import '../bloc/popular/popular_bloc.dart';
+import '../bloc/toprated/top_rated_bloc.dart';
 import 'movie_item.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,19 +14,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: BlocBuilder<TrendingBloc, TrendingState>(
-            builder: (context, state) => switch (state) {
-                  TrendingInitial() => Center(
-                      child: const Text('home.title').tr(),
-                    ),
-                  TrendingEmpty() => const Text('Empty Trending Movies list'),
-                  TrendingLoading() => const ProgressWheel(),
-                  TrendingLoaded() => ListView.builder(
-                      itemCount: state.data.length,
-                      itemBuilder: (BuildContext context, int index) =>
-                          MovieItem(data: state.data[index])),
-                  TrendingFailure() => Text(state.message),
-                }));
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (_) => sl<TrendingBloc>()..add(GetTrendingEvent())),
+          BlocProvider(
+              create: (_) => sl<PopularBloc>()..add(GetPopularEvent())),
+          BlocProvider(
+              create: (_) => sl<TopRatedBloc>()..add(GetTopRatedEvent()))
+        ],
+      child: SafeArea(
+          child: BlocBuilder<TrendingBloc, TrendingState>(
+              builder: (context, state) => switch (state) {
+                    TrendingInitial() => Center(
+                        child: const Text('home.title').tr(),
+                      ),
+                    TrendingEmpty() => const Text('Empty Trending Movies list'),
+                    TrendingLoading() => const ProgressWheel(),
+                    TrendingLoaded() => ListView.builder(
+                        itemCount: state.data.length,
+                        itemBuilder: (BuildContext context, int index) =>
+                            MovieItem(data: state.data[index])),
+                    TrendingFailure() => Text(state.message),
+                  })),
+    );
   }
 }
