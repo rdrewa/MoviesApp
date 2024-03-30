dd693fbd8acf2aa951f750e65510619534d74ceb
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-31 00:33:01 +0100

    MBA-29: Moved blocs into separated list widgets - every with dedicated bloc

--- lib/feature/home/presentation/screen/home_screen.dart
+++ lib/feature/home/presentation/screen/home_screen.dart
@@ -1,42 +1,20 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../app/di.dart';
-import '../../../common/presentation/widget/progress_wheel.dart';
-import '../bloc/trending/trending_bloc.dart';
-import '../bloc/popular/popular_bloc.dart';
-import '../bloc/toprated/top_rated_bloc.dart';
-import 'movie_item.dart';
+import '../widget/popular_movies.dart';
+import '../widget/top_rated_movies.dart';
+import '../widget/trending_movies.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider(
-              create: (_) => sl<TrendingBloc>()..add(GetTrendingEvent())),
-          BlocProvider(
-              create: (_) => sl<PopularBloc>()..add(GetPopularEvent())),
-          BlocProvider(
-              create: (_) => sl<TopRatedBloc>()..add(GetTopRatedEvent()))
-        ],
-      child: SafeArea(
-          child: BlocBuilder<TrendingBloc, TrendingState>(
-              builder: (context, state) => switch (state) {
-                    TrendingInitial() => Center(
-                        child: const Text('home.title').tr(),
-                      ),
-                    TrendingEmpty() => const Text('Empty Trending Movies list'),
-                    TrendingLoading() => const ProgressWheel(),
-                    TrendingLoaded() => ListView.builder(
-                        itemCount: state.data.length,
-                        itemBuilder: (BuildContext context, int index) =>
-                            MovieItem(data: state.data[index])),
-                    TrendingFailure() => Text(state.message),
-                  })),
-    );
+    return const SafeArea(
+        child: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [TrendingMovies(), PopularMovies(), TopRatedMovies()],
+      ),
+    ));
   }
 }
