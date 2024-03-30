c11207157fd6c1d9e52bc518547db5c240e30b3b
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-03-31 00:33:32 +0100

    MBA-29: Proper package/directory; background parametrized

--- lib/feature/home/presentation/widget/movie_item.dart
+++ lib/feature/home/presentation/widget/movie_item.dart
@@ -2,19 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '/app/app_routes.dart';
-import '/feature/common/domain/model/movie.dart';
+import '../../../common/domain/model/movie.dart';
 
 class MovieItem extends StatelessWidget {
   final Movie data;
+  final Color background;
 
-  const MovieItem({super.key, required this.data});
+  const MovieItem({super.key, required this.data, required this.background});
 
   @override
   Widget build(BuildContext context) => InkWell(
         onTap: () => context.pushNamed(AppRoutes.details,
             pathParameters: {'id': data.id.toString()}),
         child: Container(
-          color: Colors.blue,
+          color: background,
           padding: const EdgeInsets.all(4),
           margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
           child: Column(
