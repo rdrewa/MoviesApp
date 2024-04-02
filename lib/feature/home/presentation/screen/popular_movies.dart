ff354b7df7247c66ccaeb3d35a4e142c325c3f6c
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-02 15:31:13 +0200

    MBA-29: Using Skyscraper in PopularMovies

--- lib/feature/home/presentation/screen/popular_movies.dart
+++ lib/feature/home/presentation/screen/popular_movies.dart
@@ -2,11 +2,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 import '../../../../app/di.dart';
 import '../../../common/presentation/widget/progress_wheel.dart';
 import '../bloc/popular/popular_bloc.dart';
-import 'movie_item.dart';
+import '../widget/skyscraper/skyscraper_list.dart';
 
 class PopularMovies extends StatelessWidget {
   const PopularMovies({super.key});
@@ -22,16 +21,9 @@ class PopularMovies extends StatelessWidget {
                   ),
                 PopularEmpty() => const Text('Empty Popular Movies list'),
                 PopularLoading() => const ProgressWheel(),
-                PopularLoaded() => Column(
-                    children: [
-                      const Text('Popular Movies: Loaded'),
-                      ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: state.data.length,
-                          itemBuilder: (BuildContext context, int index) =>
-                              MovieItem(data: state.data[index], background: Colors.orange,))
-                    ],
+                PopularLoaded() => SkyscraperList(
+                    title: 'Popular Movie',
+                    list: state.data,
                   ),
                 PopularFailure() => Text(state.message),
               }),
