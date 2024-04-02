065d398fe427046b5824bafc1149c7360b2ec798
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-02 17:31:15 +0200

    MBA-31: Changed import

--- lib/feature/home/presentation/widget/slider/slider_list.dart
+++ lib/feature/home/presentation/widget/slider/slider_list.dart
@@ -1,35 +1,39 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:movies_app/feature/home/presentation/widget/headline_text.dart';
 
 import '../../../../common/domain/model/movie.dart';
+import '../headline_text.dart';
 import 'slider_item.dart';
 
 class SliderList extends StatelessWidget {
   final String title;
   final List<Movie> list;
+  final double height;
 
-  const SliderList({super.key, required this.title, required this.list});
+  const SliderList(
+      {super.key,
+      required this.title,
+      required this.list,
+      required this.height});
 
   @override
-  Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height * 0.35;
-    return Stack(
-      children: [
-        CarouselSlider.builder(
-            itemCount: list.length,
-            itemBuilder: (context, index, realIndex) => SliderItem(
-                item: list[index], width: double.maxFinite, onTap: () {}),
-            options: CarouselOptions(
-              viewportFraction: 1,
-              height: height,
-              // autoPlay: true,
-            )),
-        Padding(
-          padding: const EdgeInsets.only(left: 10, top: 20),
-          child: HeadlineText(text: title,),
-        ),
-      ],
-    );
-  }
+  Widget build(BuildContext context) => Stack(
+        children: [
+          CarouselSlider.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index, realIndex) => SliderItem(
+                  item: list[index], width: double.maxFinite, onTap: () {}),
+              options: CarouselOptions(
+                viewportFraction: 1,
+                height: height,
+                // autoPlay: true,
+              )),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 20),
+            child: HeadlineText(
+              text: title,
+            ),
+          ),
+        ],
+      );
 }
