b5becb621b1df70ecfb4f137b555f9925d8c6b68
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-02 17:31:03 +0200

    MBA-31: Additional parameter height

--- lib/feature/common/presentation/widget/progress_wheel.dart
+++ lib/feature/common/presentation/widget/progress_wheel.dart
@@ -4,20 +4,24 @@ class ProgressWheel extends StatelessWidget {
   final double size;
   final Color color;
   final Color backgroundColor;
+  final double? height;
 
   const ProgressWheel({super.key, this.size = 50, this.color = Colors
-      .red, this.backgroundColor = Colors.lightGreenAccent});
+      .red, this.backgroundColor = Colors.lightGreenAccent, this.height});
 
   @override
   Widget build(BuildContext context) =>
-      Center(
-        child: SizedBox(
-          height: size,
-          width: size,
-          child: CircularProgressIndicator(
-            color: color,
-            backgroundColor: backgroundColor,
-            strokeWidth: 0.15 * size,
+      SizedBox(
+        height: height,
+        child: Center(
+          child: SizedBox(
+            height: size,
+            width: size,
+            child: CircularProgressIndicator(
+              color: color,
+              backgroundColor: backgroundColor,
+              strokeWidth: 0.15 * size,
+            ),
           ),
         ),
       );
