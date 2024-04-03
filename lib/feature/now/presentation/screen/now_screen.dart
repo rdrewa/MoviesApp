24cde678e047712bb7f607ee600febb0aef82110
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-03 16:52:40 +0200

    MBA-33: Using CarouselList

--- lib/feature/now/presentation/screen/now_screen.dart
+++ lib/feature/now/presentation/screen/now_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../app/di.dart';
 import '../../../common/presentation/widget/progress_wheel.dart';
-import '../widget/now_container.dart';
+import '../widget/carousel_list.dart';
 import '../bloc/now/now_bloc.dart';
 
 class NowScreen extends StatelessWidget {
@@ -13,13 +13,12 @@ class NowScreen extends StatelessWidget {
   Widget build(BuildContext context) => BlocProvider(
         create: (context) => sl<NowBloc>()..add(GetNowEvent()),
         child: Scaffold(
-          appBar: AppBar(),
           body: BlocBuilder<NowBloc, NowState>(
             builder: (BuildContext context, NowState state) => switch (state) {
               NowInitial() => const SizedBox.shrink(),
               NowEmpty() => const Text('Empty Trending Movies list'),
               NowLoading() => const ProgressWheel(),
-              NowLoaded() => NowContainer(list: state.data),
+              NowLoaded() => CarouselList(list: state.data),
               NowFailure() => Text(state.message),
             },
           ),
