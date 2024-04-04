b428d67ebb0a15a99f7741ee2dcadb424aa207f2
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-04 16:58:07 +0200

    MBA-35: Method onTap added

--- lib/feature/now/presentation/widget/carousel_item.dart
+++ lib/feature/now/presentation/widget/carousel_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:easy_localization/easy_localization.dart';
--- lib/feature/watch/presentation/widget/watch_list_container.dart
+++ lib/feature/watch/presentation/widget/watch_list_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../app/app_routes.dart';
 import '../../../common/domain/model/movie.dart';
 import '../widget/watch_item.dart';
 
@@ -10,7 +12,10 @@ class WatchListContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
       itemCount: list.length,
-      itemBuilder: (BuildContext context, int index) =>
-          WatchItem(item: list[index]));
+      itemBuilder: (BuildContext context, int index) => WatchItem(
+          item: list[index],
+          onTap: () => context.goNamed(AppRoutes.details,
+              pathParameters: {'id': list[index].id.toString()})));
 }
