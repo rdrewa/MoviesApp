166524fb26a17849433f5422f2bce45442e93141
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-04 09:42:17 +0200

    MBA-33: Using PicturesGrid

--- lib/feature/now/presentation/widget/carousel_item.dart
+++ lib/feature/now/presentation/widget/carousel_item.dart
@@ -1,183 +1,105 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/extension/string_image_extension.dart';
 import '../../domain/model/movie_now.dart';
+import '../../domain/model/picture.dart';
+import 'pictures_grid.dart';
 
 class CarouselItem extends StatelessWidget {
   final MovieNow item;
   final double height;
 
-  CarouselItem({super.key, required this.item, required this.height});
+  const CarouselItem({super.key, required this.item, required this.height});
 
-  final colors = [
-    Colors.yellow,
-    Colors.red,
-    Colors.grey,
-    Colors.blue,
-    Colors.amber,
-    Colors.blueGrey,
-    Colors.white70,
-    Colors.indigo,
-    Colors.purple,
-    Colors.blueGrey,
-    Colors.orange,
-    Colors.purpleAccent,
-    Colors.blue,
-    Colors.amber,
-    Colors.blueGrey,
-    Colors.white70,
-    Colors.indigo,
-    Colors.purple,
-  ];
+  String _backdropProvider(Picture picture) => picture.filePath.imageW1280;
+
+  String _posterProvider(Picture picture) => picture.filePath.imageW500;
+
+  String _logoProvider(Picture picture) => picture.filePath.imageW300;
 
   @override
-  Widget build(BuildContext context) {
-    final backdrops = item.pictures.backdrops /*.take(6).toList()*/;
-    final posters = item.pictures.posters /*.take(8).toList()*/;
-    final logos = item.pictures.logos /*.take(3).toList()*/;
-    return Padding(
-      padding: const EdgeInsets.only(top: 20),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.grey,
-        ),
-        height: height,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: double.maxFinite,
-                height: 180.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: CachedNetworkImageProvider(
-                          item.backdropPath!.imageW500)),
-                  borderRadius: const BorderRadius.all(Radius.circular(12.0)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10, top: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        item.title,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium
-                            ?.copyWith(color: Colors.white),
-                      )
-                    ],
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.grey,
+          ),
+          height: height,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.maxFinite,
+                  height: 180.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: CachedNetworkImageProvider(
+                            item.backdropPath!.imageW500)),
+                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
                   ),
-                ),
-              ),
-              DefaultTabController(
-                  length: 3,
-                  child: SizedBox(
-                    height: 350,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, top: 20),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const TabBar(
-                            tabs: [
-                              Tab(
-                                text: "Backdrops",
-                              ),
-                              Tab(
-                                text: "Posters",
-                              ),
-                              Tab(
-                                text: "Logos",
-                              )
-                            ]),
-                        Expanded(
-                          child: TabBarView(
-                              physics: const NeverScrollableScrollPhysics(),
-                              children: [
-                                GridView.builder(
-                                  gridDelegate:
-                                      const SliverGridDelegateWithFixedCrossAxisCount(
-                                          crossAxisCount: 3),
-                                  physics: const ClampingScrollPhysics(),
-                                  shrinkWrap: true,
-                                  itemCount: backdrops.length,
-                                  itemBuilder: (context, index) =>
-                                      MoviePicture(
-                                          url: backdrops[index]
-                                              .filePath
-                                              .imageW500,
-                                          color:
-                                              colors[index % colors.length]),
-                                ),
-                                GridView.builder(
-                                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 3),
-                                  physics: const ClampingScrollPhysics(),
-                                  // physics: const NeverScrollableScrollPhysics(),
-                                  shrinkWrap: true,
-                                  itemCount: posters.length,
-                                  itemBuilder: (context, index) => MoviePicture(
-                                      url: posters[index].filePath.imageW500,
-                                      color: colors[index % colors.length]),
-                                ),
-                                GridView.builder(
-                                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 3),
-                                  physics: const ClampingScrollPhysics(),
-                                  // physics: const NeverScrollableScrollPhysics(),
-                                  shrinkWrap: true,
-                                  itemCount: logos.length,
-                                  itemBuilder: (context, index) => MoviePicture(
-                                      url: logos[index].filePath.imageW500,
-                                      color: colors[index % colors.length]),
-                                )
-                              ]),
+                        Text(
+                          item.title,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium
+                              ?.copyWith(color: Colors.white),
                         )
                       ],
                     ),
-                  )),
-              // Text(item.overview)
-            ],
+                  ),
+                ),
+                DefaultTabController(
+                    length: 3,
+                    child: SizedBox(
+                      height: 400,
+                      child: Column(
+                        children: [
+                          const TabBar(tabs: [
+                            Tab(
+                              text: "Backdrops",
+                            ),
+                            Tab(
+                              text: "Posters",
+                            ),
+                            Tab(
+                              text: "Logos",
+                            )
+                          ]),
+                          Expanded(
+                            child: TabBarView(
+                                physics: const NeverScrollableScrollPhysics(),
+                                children: [
+                                  PicturesGrid(
+                                      count: 3,
+                                      list: item.pictures.backdrops,
+                                      provider: _backdropProvider),
+                                  PicturesGrid(
+                                      count: 4,
+                                      list: item.pictures.posters,
+                                      provider: _posterProvider),
+                                  PicturesGrid(
+                                      count: 3,
+                                      list: item.pictures.logos,
+                                      provider: _logoProvider)
+                                ]),
+                          )
+                        ],
+                      ),
+                    )),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
-
-class MoviePicture extends StatelessWidget {
-  final String url;
-  final Color color;
-
-  const MoviePicture({super.key, required this.url, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      // width: 160,
-      // height: 150,
-      color: color,
-      child: Image(
-        image: CachedNetworkImageProvider(url),
-        // width: 160,
-        // height: 180,
-      ),
-    );
-  }
-}
-
-/*
-              GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3),
-                physics: const NeverScrollableScrollPhysics(),
-                // physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: backdrops.length,
-                itemBuilder: (context, index) => MoviePicture(
-                    url: backdrops[index].filePath.imageW500,
-                    color: colors[index % colors.length]),
-              ),
- */
