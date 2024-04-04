f9c8999a4ae441c4e9751e79a9b15cadef99f943
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-04 10:46:34 +0200

    MBA-33: Loading, Error handling added; Color removed

--- lib/feature/now/presentation/widget/movie_picture.dart
+++ lib/feature/now/presentation/widget/movie_picture.dart
@@ -3,20 +3,26 @@ import 'package:flutter/material.dart';
 
 class MoviePicture extends StatelessWidget {
   final String url;
-  final Color color;
 
-  const MoviePicture({super.key, required this.url, required this.color});
+  const MoviePicture({super.key, required this.url});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      // width: 160,
-      // height: 150,
-      color: color,
-      child: Image(
-        image: CachedNetworkImageProvider(url),
-        // width: 160,
-        // height: 180,
+      alignment: AlignmentDirectional.center,
+      child: CachedNetworkImage(
+        imageUrl: url,
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+        progressIndicatorBuilder: (context, url, progress) => Center(
+            child: CircularProgressIndicator(
+          value: progress.progress,
+        )),
+        errorWidget: (context, url, error) =>
+            const Icon(Icons.motion_photos_off_outlined),
       ),
     );
   }
