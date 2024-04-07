b58fc0973e075973658f39e9ff2fb8cbc13732f8
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-07 23:24:37 +0200

    MBA-37: Using styles from Theme via extension on BuildContext

--- lib/feature/watch/presentation/widget/watch_overview.dart
+++ lib/feature/watch/presentation/widget/watch_overview.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '/core/extension/build_context_theme_extension.dart';
+
 class WatchOverview extends StatelessWidget {
   final String overview;
 
   const WatchOverview({super.key, required this.overview});
 
   @override
-  Widget build(BuildContext context) => Text(
-        overview,
-        maxLines: 5,
-        overflow: TextOverflow.fade,
-        textAlign: TextAlign.justify,
-        style: Theme.of(context).textTheme.bodySmall,
-      );
+  Widget build(BuildContext context) => Text(overview,
+      maxLines: 5,
+      overflow: TextOverflow.fade,
+      textAlign: TextAlign.justify,
+      style: context.bodySmall);
 }
