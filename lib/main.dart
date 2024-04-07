3e9c56083947309103213824f07fe0eac4fcdec4
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-07 22:38:54 +0200

    MBA-37: Removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movies_app/feature/common/data/hive_database.dart';
 
 import '../core/util/language_util.dart';
-import '../feature/common/domain/model/movie.dart';
 import 'app/di.dart';
 import 'app/movie_app.dart';
 
