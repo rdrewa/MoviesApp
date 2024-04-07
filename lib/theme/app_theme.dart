2bc890e9203dded2a710a06f417b3b9cf5e42ce4
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-07 22:33:08 +0200

    MBA-37: Applied colorScheme & textTheme on AppTheme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final latoTextTheme = GoogleFonts.latoTextTheme();
+import 'app_color_scheme.dart';
 
-final ThemeData appTheme = ThemeData(
-    brightness: Brightness.light,
-    textTheme: GoogleFonts.latoTextTheme().apply(
-      bodyColor: Colors.orange,
-      displayColor: Colors.blue,
-    ).copyWith(
-      displayMedium: latoTextTheme.displayMedium?.copyWith(color: Colors.white)
-    )
-);
+final TextTheme fontTextTheme = GoogleFonts.latoTextTheme();
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightMode = ThemeData.light(useMaterial3: true).copyWith(
+      brightness: Brightness.light,
+      colorScheme: AppColorScheme.lightScheme,
+      scaffoldBackgroundColor: AppColorScheme.lightScheme.background,
+      textTheme: fontTextTheme
+          .apply(
+              bodyColor: AppColorScheme.lightScheme.onBackground,
+              displayColor: AppColorScheme.lightScheme.primary)
+          .copyWith(
+              labelMedium: fontTextTheme.labelMedium
+                  ?.copyWith(color: AppColorScheme.lightScheme.tertiary)));
+
+  static ThemeData darkMode = ThemeData.dark(useMaterial3: true).copyWith(
+      brightness: Brightness.dark,
+      colorScheme: AppColorScheme.darkScheme,
+      scaffoldBackgroundColor: AppColorScheme.darkScheme.background,
+      textTheme: fontTextTheme
+          .apply(
+              bodyColor: AppColorScheme.darkScheme.onBackground,
+              displayColor: AppColorScheme.darkScheme.primary)
+          .copyWith(
+              labelMedium: fontTextTheme.labelMedium
+                  ?.copyWith(color: AppColorScheme.darkScheme.tertiary)));
+}
