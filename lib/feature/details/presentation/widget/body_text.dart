a8b992e37c5a06fca37e2637620d02d298810ff5
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-08 18:07:57 +0200

    MBA-39: Converted into more reusable widget

--- lib/feature/details/presentation/widget/body_text.dart
+++ lib/feature/details/presentation/widget/body_text.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 import '/core/extension/build_context_theme_extension.dart';
 
-class DescriptionText extends StatelessWidget {
-  final String description;
+class BodyText extends StatelessWidget {
+  final String body;
 
-  const DescriptionText({super.key, required this.description});
+  const BodyText({super.key, required this.body});
 
   @override
   Widget build(BuildContext context) => Container(
       padding: const EdgeInsets.symmetric(vertical: 12),
-      child: Text(description,
+      child: Text(body,
           style: context.bodyMedium, textAlign: TextAlign.justify));
 }
