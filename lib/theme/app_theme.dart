e32f04e3cf22ca1d3ac570f227246694db35f42b
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-08 11:58:27 +0200

    MBA-37: Using themes for AppBar, BottomNavigationBar, TabView & Text

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
+import 'app_tab_bar_theme.dart';
+import 'app_app_bar_theme.dart';
+import 'app_bottom_bar_theme.dart';
+import 'app_text_theme.dart';
 import 'app_color_scheme.dart';
-
-final TextTheme fontTextTheme = GoogleFonts.latoTextTheme();
 
 class AppTheme {
   AppTheme._();
@@ -12,23 +13,17 @@ class AppTheme {
       brightness: Brightness.light,
       colorScheme: AppColorScheme.lightScheme,
       scaffoldBackgroundColor: AppColorScheme.lightScheme.background,
-      textTheme: fontTextTheme
-          .apply(
-              bodyColor: AppColorScheme.lightScheme.onBackground,
-              displayColor: AppColorScheme.lightScheme.primary)
-          .copyWith(
-              labelMedium: fontTextTheme.labelMedium
-                  ?.copyWith(color: AppColorScheme.lightScheme.tertiary)));
+      textTheme: AppTextTheme.lightTheme,
+      appBarTheme: AppAppBarTheme.lightTheme,
+      tabBarTheme: AppTabBarTheme.lightTheme,
+      bottomNavigationBarTheme: AppBottomBapTheme.lightTheme);
 
   static ThemeData darkMode = ThemeData.dark(useMaterial3: true).copyWith(
       brightness: Brightness.dark,
       colorScheme: AppColorScheme.darkScheme,
       scaffoldBackgroundColor: AppColorScheme.darkScheme.background,
-      textTheme: fontTextTheme
-          .apply(
-              bodyColor: AppColorScheme.darkScheme.onBackground,
-              displayColor: AppColorScheme.darkScheme.primary)
-          .copyWith(
-              labelMedium: fontTextTheme.labelMedium
-                  ?.copyWith(color: AppColorScheme.darkScheme.tertiary)));
+      textTheme: AppTextTheme.darkTheme,
+      appBarTheme: AppAppBarTheme.darkTheme,
+      tabBarTheme: AppTabBarTheme.darkTheme,
+      bottomNavigationBarTheme: AppBottomBapTheme.darkTheme);
 }
