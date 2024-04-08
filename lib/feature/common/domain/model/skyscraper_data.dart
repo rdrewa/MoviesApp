a297635f80e0b5379b5bd46eb6c1879168018069
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-08 16:51:31 +0200

    MBA-39: Abstract class (~ interface) for reusable SkyscraperList & SkyscraperItem

---
+++ lib/feature/common/domain/model/skyscraper_data.dart
@@ -0,0 +1,13 @@
+import 'dart:core';
+
+abstract class SkyscraperData {
+  abstract int identifier;
+
+  abstract String label;
+
+  abstract String? imageUrl;
+
+  abstract double vote;
+
+  abstract bool hasVote;
+}
