934c662c4e12fa27bc41e0db5a2ac0e5f32274e3
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-09 16:27:29 +0200

    MBA-41: Mocks for DetailsBloc & WatchEditBloc added

--- test/util/mocks.dart
+++ test/util/mocks.dart
@@ -10,6 +10,8 @@ import 'package:movies_app/feature/details/domain/usecase/add_to_watch_usecase.d
 import 'package:movies_app/feature/details/domain/usecase/get_details_usecase.dart';
 import 'package:movies_app/feature/details/domain/usecase/is_on_watch_usecase.dart';
 import 'package:movies_app/feature/details/domain/usecase/remove_from_watch_usecase.dart';
+import 'package:movies_app/feature/details/presentation/bloc/details/details_bloc.dart';
+import 'package:movies_app/feature/details/presentation/bloc/watchedit/watch_edit_bloc.dart';
 import 'package:movies_app/feature/home/domain/usecase/get_popular_usecase.dart';
 import 'package:movies_app/feature/home/domain/usecase/get_top_rated_usecase.dart';
 import 'package:movies_app/feature/home/domain/usecase/get_trending_usecase.dart';
@@ -61,4 +63,11 @@ class MockTopRatedBloc extends MockBloc<TopRatedEvent, TopRatedState>
 
 class MockNowBloc extends MockBloc<NowEvent, NowState> implements NowBloc {}
 
-class MockWatchListBloc extends MockBloc<WatchListEvent, WatchListState> implements WatchListBloc {}
+class MockWatchListBloc extends MockBloc<WatchListEvent, WatchListState>
+    implements WatchListBloc {}
+
+class MockDetailsBloc extends MockBloc<DetailsEvent, DetailsState>
+    implements DetailsBloc {}
+
+class MockWatchEditBloc extends MockBloc<WatchEditEvent, WatchEditState>
+    implements WatchEditBloc {}
