5ee01919aff1ea1f88da5eda270c8bd516687afb
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-10 07:53:07 +0200

    MBA-43: Added missing state

--- lib/feature/watch/presentation/bloc/list/watch_list_bloc.dart
+++ lib/feature/watch/presentation/bloc/list/watch_list_bloc.dart
@@ -18,6 +18,8 @@ class WatchListBloc extends Bloc<WatchListEvent, WatchListState> {
 
   FutureOr<void> _onGetWatchListEvent(
       GetWatchListEvent event, Emitter<WatchListState> emit) async {
+    emit(WatchListLoading());
+
     (await getWatchListUsecase()).fold(
         (fail) => emit(WatchListFailure(fail.message)),
         (data) =>
