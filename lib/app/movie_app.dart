17479e038fbca561ab6dc543303bf1c17b551f96
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-10 12:18:34 +0200

    MBA-45: Screenshots added

--- lib/app/movie_app.dart
+++ lib/app/movie_app.dart
@@ -17,7 +17,7 @@ class MovieApp extends StatelessWidget {
       locale: context.locale,
       theme: AppTheme.lightMode,
       darkTheme: AppTheme.darkMode,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
     );
   }
 }
