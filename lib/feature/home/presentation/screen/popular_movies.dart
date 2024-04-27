ab5c7b9cdff5927ee77644c903c965304a504e68
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-27 23:51:57 +0200

    MBA-48: Routing for PopularPaginated

--- lib/feature/home/presentation/screen/popular_movies.dart
+++ lib/feature/home/presentation/screen/popular_movies.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../app/app_routes.dart';
 import '../../../../app/di.dart';
 import '../../../common/presentation/widget/empty_box.dart';
 import '../../../common/presentation/widget/error_box.dart';
@@ -24,6 +25,7 @@ class PopularMovies extends StatelessWidget {
                 PopularLoaded() => SkyscraperList(
                     title: 'home.popular.title'.tr(),
                     list: state.data,
+                    route: AppRoutes.popular,
                   ),
                 PopularFailure() => ErrorBox(message: state.message),
               }),
