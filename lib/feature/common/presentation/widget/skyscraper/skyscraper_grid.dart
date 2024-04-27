b59e233bfaa6550c68497f2bb27cda23a52e17f6
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-27 23:54:25 +0200

    MBA-48: Show properly grid

--- lib/feature/common/presentation/widget/skyscraper/skyscraper_grid.dart
+++ lib/feature/common/presentation/widget/skyscraper/skyscraper_grid.dart
@@ -3,7 +3,6 @@ import 'package:go_router/go_router.dart';
 
 import '../../../../../app/app_routes.dart';
 import '../../../domain/model/skyscraper_data.dart';
-import '../headline_text.dart';
 import 'skyscraper_item.dart';
 
 class SkyscraperGrid extends StatelessWidget {
@@ -15,42 +14,15 @@ class SkyscraperGrid extends StatelessWidget {
       {super.key, required this.title, required this.list, this.height = 250});
 
   @override
-  Widget build(BuildContext context) {
-    if (list.isEmpty) return const SizedBox.shrink();
-
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          HeadlineText(text: title),
-          const SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            height: height,
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: list.length,
-                    mainAxisSpacing: 2,
-                    crossAxisSpacing: 4),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: list.length,
-                itemBuilder: (BuildContext context, int index) =>
-                    SkyscraperItem(
-                        item: list[index],
-                        width: 120,
-                        onTap: () => context.goNamed(AppRoutes.details,
-                                pathParameters: {
-                                  'id': list[index].identifier.toString()
-                                }))),
-          )
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, childAspectRatio: 0.5),
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      itemCount: list.length,
+      itemBuilder: (BuildContext context, int index) => SkyscraperItem(
+          item: list[index],
+          width: 120,
+          onTap: () => context.goNamed(AppRoutes.details,
+              pathParameters: {'id': list[index].identifier.toString()})));
 }
