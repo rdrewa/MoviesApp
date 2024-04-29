35054fbf6831ff6fce43709872b2645b9a4b7d27
Radosław Drewa <radoslaw.drewa@nextapps.pl>
2024-04-29 09:03:24 +0200

    MBA-48: Showing little ProgressWheel on LoadingMore

--- lib/feature/common/presentation/widget/skyscraper/skyscraper_grid.dart
+++ lib/feature/common/presentation/widget/skyscraper/skyscraper_grid.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:movies_app/feature/common/presentation/widget/progress_wheel.dart';
 
 import '../../../../../app/app_routes.dart';
 import '../../../domain/model/skyscraper_data.dart';
@@ -8,10 +9,13 @@ import 'skyscraper_item.dart';
 class SkyscraperGrid extends StatelessWidget {
   final String title;
   final List<SkyscraperData> list;
-  final double height;
+  final bool hasMore;
 
   const SkyscraperGrid(
-      {super.key, required this.title, required this.list, this.height = 250});
+      {super.key,
+      required this.title,
+      required this.list,
+      required this.hasMore});
 
   @override
   Widget build(BuildContext context) => GridView.builder(
@@ -19,10 +23,14 @@ class SkyscraperGrid extends StatelessWidget {
           crossAxisCount: 3, childAspectRatio: 0.5),
       shrinkWrap: true,
       scrollDirection: Axis.vertical,
-      itemCount: list.length,
-      itemBuilder: (BuildContext context, int index) => SkyscraperItem(
-          item: list[index],
-          width: 120,
-          onTap: () => context.goNamed(AppRoutes.details,
-              pathParameters: {'id': list[index].identifier.toString()})));
+      itemCount: list.length + (hasMore ? 1 : 0),
+      itemBuilder: (BuildContext context, int index) => (index >= list.length)
+          ? const ProgressWheel(
+              height: 150,
+            )
+          : SkyscraperItem(
+              item: list[index],
+              width: 120,
+              onTap: () => context.goNamed(AppRoutes.details,
+                  pathParameters: {'id': list[index].identifier.toString()})));
 }
